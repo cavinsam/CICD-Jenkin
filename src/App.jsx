158a386543bf2bc33e44184370009cdf019368af
cavinsam <cavinsam.26@gmail.com>
2025-08-09 10:39:15 +0530

    Edit

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>CICD via Jenkin</h1>
+      <h2>My name is Cavin</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
